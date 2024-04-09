240e2886d6daf5ca7737d79ce2aa1183f0b41c05
Usman Awan <usman@norbay.no>
2024-04-09 15:50:03 +0200

    JIRA-1: Opprettet nytt prosjekt i intelliJ

---
+++ src/main/kotlin/no/nlod/arbeidsplassen/ArbeidsplassenApiApplication.kt
@@ -0,0 +1,11 @@
+package no.nlod.arbeidsplassen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArbeidsplassenApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArbeidsplassenApiApplication>(*args)
+}
